5ae4bfe49e677e2d177f4a0b945ee42397d9cd18
steve <steve.kim@enhanceconsulting.co.nz>
2021-07-02 08:14:56 +1200

    pull from main branch

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +12,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
-    public class StudentController {
-        @GetMapping
-        public List<Student> getStudents() {
-            return List.of(
-                    new Student(
-                            1L,
-                            "Mariam",
-                            "mariam.jamal@gmail.com",
-                            LocalDate.of(2000, Month.JANUARY, 5),
-                            21
-                    )
-            );
-        }
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
     }
+}
