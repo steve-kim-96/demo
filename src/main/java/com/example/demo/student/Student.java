eee0fb6f0366f32d60c48b75b994965668a900f4
steve <steve.kim@enhanceconsulting.co.nz>
2021-07-19 18:07:15 +1200

    including lombok in dependency and deleting all unnecessary code

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,10 +1,16 @@
 package com.example.demo.student;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table
 public class Student {
@@ -26,62 +32,22 @@ public class Student {
     @Transient
     private Integer age;
 
-    public Student() {
-    }
-
     public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 
     public Integer getAge() {
         return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
     }
 
     @Override
